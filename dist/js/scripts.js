5277cbf6c96fb5fde99bdcdcc8e05ce1e76345f4
Chase Groom <groom.chase@gmail.com>
2022-03-25 16:27:40 -0700

    a lot

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -5,3 +5,10 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+import AOS from 'aos';
+AOS = require('aos');
+import 'aos/dist/aos.css';
+AOS.init({
+    duration: 1200,
+  })
+  
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,9 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+import AOS from 'aos';
+AOS = require('aos');
+import 'aos/dist/aos.css';
+AOS.init({
+    duration: 1200,
+  })
+  
